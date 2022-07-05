d76051f3e1368f6693e3f4eb8628d4adf3f885bf
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-05 19:09:42 -0300

    Index JS created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import First from "./components/basic/First";
+import WithParameter from "./components/basic/WithParameter";
+
+ReactDOM.render(
+  <div>
+    <First />
+    <WithParameter
+      title="Situação do aluno"
+      aluno="Carlos"
+      nota={6.4}
+    />
+    <WithParameter
+      title="Situação do aluno"
+      aluno="Maria"
+      nota={8.9}
+    />
+  </div>,
+  document.getElementById("root")
+);
