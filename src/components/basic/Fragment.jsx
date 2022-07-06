c72eae08f9b2937dbaed0975a7ed6e315c92de4d
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-06 15:22:57 -0300

    FragmentJSX changed

--- src/components/basic/Fragment.jsx
+++ src/components/basic/Fragment.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Fragment(props){
+export default function(props){
     return(
-        <>
+        <React.Fragment>
             <h2>Fragmento</h2>
             <p>Cuidado com esse erro</p>
-        </>
+        </React.Fragment>
     )
 }
