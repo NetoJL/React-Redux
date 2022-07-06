20d156cf848877395e8a7ad3311b78fee6f8060a
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-06 15:38:26 -0300

    AppCSS added to AppJSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './App.css'
+
 import First from "./components/basic/First";
 import WithParameter from "./components/basic/WithParameter";
 import Fragment from "./components/basic/Fragment";
@@ -7,19 +9,25 @@ import Random from "./components/basic/Random";
 import Card from "./components/layout/Card";
 
 export default () => (
-  <div id="app">
-    <h1>Fundamentos React</h1>
-    <Card title="#04 - Desafio aleatório">
-      <Random max={100} min={10} />
-    </Card>
-    <Card title="#03 - Fragment">
-      <Fragment />
-    </Card>
-    <Card title="#02 - With Parameter">
-      <WithParameter title="Situação do aluno" aluno="Carlos" nota={6.4} />
-    </Card>
-    <Card title="#01 - First component">
-      <First />
-    </Card>
-  </div>
+	<div className="App">
+		<h1>Fundamentos React</h1>
+		<div className="Cards">
+			<Card title="#04 - Desafio aleatório">
+				<Random max={100} min={10} />
+			</Card>
+			<Card title="#03 - Fragment">
+				<Fragment />
+			</Card>
+			<Card title="#02 - With Parameter">
+				<WithParameter
+					title="Situação do aluno"
+					aluno="Carlos"
+					nota={6.4}
+				/>
+			</Card>
+			<Card title="#01 - First component">
+				<First />
+			</Card>
+		</div>
+	</div>
 );
