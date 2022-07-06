d06b4547b96af4de8a4cd518c5253cfb83d75360
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-06 17:51:51 -0300

    Card (JSX and CSS) changed

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-import './Card.css'
+import "./Card.css";
 
-export default props => {
-    return(
-        <div className="Card">
-            <div className="Title">{props.title}</div>
-            <div className="Content">{props.children}</div>
-        </div>
-    );
-}
+export default (props) => {
+	return (
+		<div
+			className="Card"
+			style={{
+				backgroundColor: props.color || "#F00",
+				borderColor: props.color || "#F00",
+			}}>
+			<div className="Title">{props.title}</div>
+			<div className="Content">{props.children}</div>
+		</div>
+	);
+};
