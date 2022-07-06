24840f0271afc5a3e683c8fc6ced36ab11ce162a
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-06 17:50:23 -0300

    FamilyJSX added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 
-import './App.css'
+import "./App.css";
 
 import First from "./components/basic/First";
 import WithParameter from "./components/basic/WithParameter";
 import Fragment from "./components/basic/Fragment";
 import Random from "./components/basic/Random";
 import Card from "./components/layout/Card";
+import Family from "./components/basic/Family";
 
 export default () => (
 	<div className="App">
 		<h1>Fundamentos React</h1>
 		<div className="Cards">
-			<Card title="#04 - Desafio aleatório">
+			<Card title="#05 - Component whit childrens" color="#00C8F8">
+				<Family lastName="Ferreira" />
+			</Card>
+			<Card title="#04 - Random challenge" color="#FA6900">
 				<Random max={100} min={10} />
 			</Card>
-			<Card title="#03 - Fragment">
+			<Card title="#03 - Fragment" color="#E94C6F">
 				<Fragment />
 			</Card>
-			<Card title="#02 - With Parameter">
+			<Card title="#02 - With Parameter" color="#E8B71A">
 				<WithParameter
 					title="Situação do aluno"
 					aluno="Carlos"
 					nota={6.4}
 				/>
 			</Card>
-			<Card title="#01 - First component">
+			<Card title="#01 - First component" color="#588C73">
 				<First />
 			</Card>
 		</div>
