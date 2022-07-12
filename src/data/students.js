e649e090195f875d1cba4b5ec7a244b1428ce7ac
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-12 20:52:01 -0300

    Student data created

---
+++ src/data/students.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, nome: 'José', nota: 9.2 },
+    { id: 2, nome: 'Bia', nota: 7.6 },
+    { id: 3, nome: 'Carlos', nota: 98.1 },
+    { id: 4, nome: 'Daniel', nota: 5.3 },
+    { id: 5, nome: 'Gui', nota: 2.9 },
+    { id: 6, nome: 'Rebeca', nota: 7.6 },
+    { id: 7, nome: 'Arthur', nota: 5.4 },
+    { id: 8, nome: 'Pedro', nota: 9.5 },
+    { id: 9, nome: 'Gustavo', nota: 4.7 }
+]
