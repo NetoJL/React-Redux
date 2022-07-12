b24611b904e69172ab94d9a6bc34539588328ac1
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-12 20:58:26 -0300

    Item data created

---
+++ src/data/items.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: 'rice', price: 2.00},
+    {id: 2, name: 'beans', price: 3.00},
+    {id: 3, name: 'potato', price: 2.50},
+    {id: 4, name: 'meat', price: 20.00},
+    {id: 5, name: 'chicken', price: 10.00},
+]
