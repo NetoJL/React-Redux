16dc36438d1fe0c16b8264231cf07e33795acef6
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-13 23:06:49 -0300

    ItemsListJSX updated

--- src/components/repetitions/ItemsList.jsx
+++ src/components/repetitions/ItemsList.jsx
@@ -1,44 +1,43 @@
 import React from "react";
 import items from "../../data/items";
+import "./itemsList.css";
 
 export default (props) => {
-	const idList = items.map((item) => {
+	const itemsList = items.map((item) => {
 		return (
-			<tr>
+			<tr className={item.id % 2 == 0 ? 'Grey' : ''}>
 				<td>{item.id}</td>
-			</tr>
-		);
-	});
-
-	const nameList = items.map((item) => {
-		return (
-			<tr>
 				<td>{item.name}</td>
+				<td>R${item.price}</td>
 			</tr>
 		);
 	});
 
-	const priceList = items.map((item) => {
-		return (
-			<tr>
-				<td>{item.price}</td>
-			</tr>
-		);
-	});
+	/*function getLines() {
+		return items.map((item) => {
+			return (
+				<tr>
+					<td>{item.id}</td>
+					<td>{item.name}</td>
+					<td>R${item.price}</td>
+				</tr>
+			);
+		});
+	}*/
 
 	return (
-		<div style={{display: 'flex', justifyContent: 'center'}}>
+		<div className="ItemsTable">
 			<table>
-				<tr>
-					<th>id</th>
-					<th>name</th>
-					<th>price</th>
-				</tr>
-				<tr>
-					<td>{idList}</td>
-					<td>{nameList}</td>
-					<td>{priceList}</td>
-				</tr>
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{itemsList}
+				</tbody>
 			</table>
 		</div>
 	);
