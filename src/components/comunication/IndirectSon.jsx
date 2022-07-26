181e52d22fe5a69adb477ee6c8a4bf33af02ea92
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-26 17:03:58 -0300

    IndirectSonJSX updated

--- src/components/comunication/IndirectSon.jsx
+++ src/components/comunication/IndirectSon.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-import IndirectSon from "./IndirectSon";
-
 export default props => {
     return (
         <div>
-            <IndirectSon name={'Marcus'} age={20} nerd={true}></IndirectSon>
-            <IndirectSon name={'Johnson'} age={17} nerd={false}></IndirectSon>
+            Filho
         </div>
     )
 }
