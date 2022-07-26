f9de84fa8c983c884df5932ce1a16cc8ede65b67
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-26 17:04:18 -0300

    Component "IndirectFather" created

---
+++ src/components/comunication/IndirectFather.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import IndirectSon from "./IndirectSon";
+
+export default props => {
+    return (
+        <div>
+            Pai
+
+            <IndirectSon></IndirectSon>
+        </div>
+    )
+}
