27bd37a81c3ea0b75eb2e2a16e4fbece67a2ab49
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-27 16:47:35 -0300

    IndirectSonJSX updated

--- src/components/comunication/IndirectSon.jsx
+++ src/components/comunication/IndirectSon.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 export default props => {
+    const min = 40
+    const max = 70
+    const ageGenerator = () => parseInt(Math.random() * (max-min) + min)
+    const nerdGenerator = () => Math.random() > 0.5
     return (
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={
+            <button style={{width: 300, height: 40 , borderRadius: 10, backgroundColor: '#000', color: 'white'}} onClick={
                 function (e) {
-                    props.onClick('José', 49, false)
+                    props.onClick('José', ageGenerator(), nerdGenerator())
                 }
             }>Fornecer informações</button>
         </div>
