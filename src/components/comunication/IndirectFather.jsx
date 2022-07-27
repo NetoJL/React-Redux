affdc66bda4823f12ce8c00e3be153240a4af8d2
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-27 16:47:51 -0300

    useState added to IndirectFatherJSX

--- src/components/comunication/IndirectFather.jsx
+++ src/components/comunication/IndirectFather.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IndirectSon from "./IndirectSon";
 
 export default props => {
 
-    let name = '?'
-    let age = 0
-    let nerd = false
+    const [name, setName] = useState('?')
+    const [age, setAge] = useState(0)
+    const [nerd, setNerd] = useState(false)
 
     function provideInfos(name, age, nerd){
-        console.log(name,age,nerd)
+        setName(name)
+        setAge(age)
+        setNerd(nerd)
     }
 
     return (
