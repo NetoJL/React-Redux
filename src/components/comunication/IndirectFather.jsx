c720d1c30d135efaf02ee1abf621d6e9b7a88f3e
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-07-27 16:25:57 -0300

    IndirectFatherJSX updated

--- src/components/comunication/IndirectFather.jsx
+++ src/components/comunication/IndirectFather.jsx
@@ -3,11 +3,23 @@ import React from "react";
 import IndirectSon from "./IndirectSon";
 
 export default props => {
+
+    let name = '?'
+    let age = 0
+    let nerd = false
+
+    function provideInfos(name, age, nerd){
+        console.log(name,age,nerd)
+    }
+
     return (
         <div>
-            Pai
-
-            <IndirectSon></IndirectSon>
+            <div>
+                <span>{name} </span>
+                <span>{age} </span>
+                <span>{nerd ? 'True' : 'False'}</span>
+            </div>
+            <IndirectSon onClick={provideInfos}></IndirectSon>
         </div>
     )
 }
