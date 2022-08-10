f2badeabc210ce2865852638179675b15c894eb4
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-08-10 17:37:45 -0300

    CounterJSX updated

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import './counter.css'
+class Counter extends Component {
+	state = {
+		number: this.props.initialNumber || 0,
+		pass: this.props.initialPass || 5,
+	};
 
-export default props =>{
+	inc = () => {
+		this.setState({
+			number: this.state.number + this.state.pass,
+		});
+	};
 
-    const [number, setNumber] = useState(0)
+	dec = () => {
+		this.setState({
+			number: this.state.number - this.state.pass,
+		});
+	};
 
-    function numberAtt(initialNumber){
-        setNumber(initialNumber)
-    }
+	setPass = (e) => {
+		this.setState({
+			pass: +e.target.value,
+		});
+	};
 
-    return(
-        <div>
-            <h2>Counter</h2>
-            <p>Initial number: {number}</p>
-        </div>
-    )
+	render() {
+		return (
+			<div className="Counter">
+				<h2>Counter</h2>
+				<h3>{this.state.number}</h3>
+				<div>
+					<label htmlFor="inputPass">Pass: </label>
+					<input
+						id="inputPass"
+						type="number"
+						value={this.state.pass}
+						onChange={this.setPass}
+					/>
+				</div>
+				<button onClick={this.inc}>+</button>
+				<button onClick={this.dec}>
+					-
+				</button>
+			</div>
+		);
+	}
 }
+
+export default Counter;
