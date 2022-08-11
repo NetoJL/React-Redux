f402c5c705f899c4eaac85047184a2c9cc6a0d9c
José Neto <56655916+NetoJL@users.noreply.github.com>
2022-08-11 17:25:53 -0300

    Conponent "PassForm" created

---
+++ src/components/counter/PassForm.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default (props) => {
+	return (
+		<div>
+			<label htmlFor="inputPass">Pass: </label>
+			<input
+				id="inputPass"
+				type="number"
+				value={props.pass}
+				onChange={e => props.setPass(+e.target.value)}
+			/>
+		</div>
+	);
+};
